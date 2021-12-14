642ad524e2b97630153917e9e0d5b9e56234f6ec
Jan Redmann <janredmann1@gmail.com>
2021-12-14 15:23:55 +0100

    add the supermarket queue

---
+++ 6kyu/Supermarket Queue.js
@@ -0,0 +1,31 @@
+/*
+https://www.codewars.com/kata/57b06f90e298a7b53d000a86
+
+There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the 
+total time required for all the customers to check out!
+input
+customers: an array of positive integers representing the queue. Each integer represents a customer, and its value 
+is the amount of time they require to check out.
+n: a positive integer, the number of checkout tills.
+output
+The function should return an integer, the total time required.
+queueTime([5,3,4], 1)
+should return 12
+because when there is 1 till, the total time is just the sum of the times
+queueTime([10,2,3,3], 2)
+should return 10
+because here n=2 and the 2nd, 3rd, and 4th people in the 
+queue finish before the 1st person has finished.
+*/
+
+const queueTime = (customers, registers) => {
+	let result = [];
+	for (let i = 0; i < registers; i++) {
+		result[i] = 0;
+	}
+	for (let customer of customers) {
+		result[0] += customer;
+		result.sort((a, b) => a - b);
+	}
+	return result[result.length - 1];
+}
